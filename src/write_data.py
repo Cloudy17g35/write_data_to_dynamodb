b174bbd9e9c516beef604b1bce6be78c90fe6f08
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2022-12-27 11:46:59 +0100

    storing table name in variable

--- src/write_data.py
+++ src/write_data.py
@@ -5,10 +5,11 @@ PATH_TO_FILE:Path = Path('data', 'sample.csv')
 
 
 def write_data_to_albums_table():
-    
+    '''writes data to Albums table in DynamoDB'''
+    table_name:str = 'Albums'
     wr.dynamodb.put_csv(
     PATH_TO_FILE,
-    table_name='Albums'
+    table_name=table_name
     )
 
 
